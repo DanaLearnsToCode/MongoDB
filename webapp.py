a2d71f17cb06e77a7c6b21187ab3565fc60c05d7
DanaLearnsToCode <77691366+DanaLearnsToCode@users.noreply.github.com>
2021-05-03 10:17:23 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,6 +3,12 @@ import os
 import sys
 import pprint
 
+post =  {"name": "bob",
+         "Age": "36",
+         "height": "5'11""}        
+
+          
+
 def main():
     connection_string = os.environ["MONGO_CONNECTION_STRING"]
     db_name = os.environ["MONGO_DBNAME"]
@@ -12,9 +18,17 @@ def main():
     collection = db['LearningDatabases'] #1. put the name of your collection in the quotes
     
     #2. add a document to your collection using the insert_one method
-    
+def insert_one():
+    posts = db.posts
+    post_id = posts.insert_one(post).inserted_id
+    post_id
+         
+         
+
+
     #3. print the number of documents in the collection
-    
+    db.list_collection_names(post)
+['posts']
     #4. print the first document in the collection
     
     #5. print all documents in the collection
