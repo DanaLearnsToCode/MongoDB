fe535789e49b49eaf1b3b45c682695582f2ae445
DanaLearnsToCode <77691366+DanaLearnsToCode@users.noreply.github.com>
2021-05-04 09:42:22 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -15,22 +15,21 @@ def main():
     
     client = pymongo.MongoClient(connection_string)
     db = client[db_name]
-    collection = db['LearningDatabases'] #1. put the name of your collection in the quotes
+    collection = db['LearningClusters'] #1. put the name of your collection in the quotes
     
     #2. add a document to your collection using the insert_one method
-def insert_one():
-    posts = db.posts
-    post_id = posts.insert_one(post).inserted_id
-    post_id
+
+    collection.insert_one(post)
+   
          
          
 
 
     #3. print the number of documents in the collection
-    db.list_collection_names(post)
-['posts']
-    #4. print the first document in the collection
+    print(collection.count())
     
+    #4. print the first document in the collection
+    print(collection.findOne())
     #5. print all documents in the collection
     
     #6. print all documents with a particular value for some attribute
